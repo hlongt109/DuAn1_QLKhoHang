0e324ea2e79ec538f0f6e2c452164f6a42113598
Hoang Long <145848843+hlongt109@users.noreply.github.com>
2023-10-14 10:50:29 +0700

    inint commit

---
+++ app/src/main/java/com/longthph30891/duan1_qlkhohang/DAO/userDAO.java
@@ -0,0 +1,7 @@
+package com.longthph30891.duan1_qlkhohang.DAO;
+
+public class userDAO {
+    public void getUserByName(String username){
+        String
+    }
+}
--- app/src/main/java/com/longthph30891/duan1_qlkhohang/database/DBHelper.java
+++ app/src/main/java/com/longthph30891/duan1_qlkhohang/database/DBHelper.java
@@ -12,6 +12,18 @@ public class DBHelper extends SQLiteOpenHelper {
     // tao bang Bill
     String create_table_bill = "Create table Bill (id integer primary key autoincrement, quantity integer, createdByUser text, createdDate text, note text)";
     // tao bang user
+    String CREATE_USER_TABLE = "CREATE TABLE " + "USER" +" (" +
+            "username" + " TEXT PRIMARY KEY," +
+            "password" + " TEXT," +
+            "numberphone" + " TEXT," +
+            "position" + " INTEGER," +
+            "avatar" + " TEXT," +
+            "profile" + " TEXT," +
+            "lastLogin" + " TEXT," +
+            "createDate" + " TEXT," +
+            "lastAction" + " TEXT" +
+            ")";
+    String dataus = "INSERT INTO USER VALUES('longthph30891','longthph30891','0869070822','1','Long - admin','14/10/2023','13/10/2023','Thêm hóa đơn')";
 
     // tao bang product
     String create_table_product = "Create table Product(id integer primary key autoincrement, name text , quantity integer, price text, photo text, userID text)";
@@ -26,6 +38,9 @@ public class DBHelper extends SQLiteOpenHelper {
         db.execSQL(create_table_bill);
         db.execSQL(create_table_product);
         db.execSQL(create_table_billDetail);
+        db.execSQL(CREATE_USER_TABLE);
+        // tạo dữ liệu
+        db.execSQL(dataus);
     }
 
     @Override
