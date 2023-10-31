74748ef0da93e31c43c1c231ea3c64b3614c6fee
Hoang Long <145848843+hlongt109@users.noreply.github.com>
2023-10-31 20:31:20 +0700

    inint commit

---
+++ app/src/main/java/com/longthph30891/duan1_qlkhohang/Activities/activitiesCreate/CreateMainActivity.java
@@ -0,0 +1,16 @@
+package com.longthph30891.duan1_qlkhohang.Activities.activitiesCreate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.longthph30891.duan1_qlkhohang.R;
+
+public class CreateMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_main);
+    }
+}
